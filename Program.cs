041729e316b3d663070cbe36359e18b808b45662
Nikiths1 <nikith.s1@mphasis.com>
2023-07-13 09:44:17 +0530

    First commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter marks in Pre: ");
+        int preMarks = Convert.ToInt32(Console.ReadLine());
+
+        if (preMarks > 60)
+        {
+            Console.Write("Enter marks in Final: ");
+            int finalMarks = Convert.ToInt32(Console.ReadLine());
+
+            if (finalMarks > 55)
+            {
+                Console.WriteLine("Selected");
+            }
+            else
+            {
+                Console.WriteLine("Fail in Final");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Fail in Pre");
+        }
+
+        Console.ReadLine();
+    }
+}
